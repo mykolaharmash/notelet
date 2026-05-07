149b35947de3d968026a0216fd7a3d82fe552b52
Mykola Harmash <mykola.harmash@gmail.com>
2026-05-07 11:57:11 +0200

    Built-in logic to auto-show current version notes

--- Sources/Notelet/NoteletSheet.swift
+++ Sources/Notelet/NoteletSheet.swift
@@ -13,10 +13,44 @@ struct NoteletSheet: ViewModifier {
     let item: Binding<NoteletSheetItem?>
     let onDismiss: () -> Void
     let configuration: NoteletConfiguration
+
+    private var presentableItem: Binding<NoteletSheetItem?> {
+        Binding(
+            get: {
+                guard let item = item.wrappedValue, shouldPresent(item) else {
+                    return nil
+                }
+
+                return item
+            },
+            set: { newValue in
+                item.wrappedValue = newValue
+            }
+        )
+    }
+
+    private func shouldPresent(_ item: NoteletSheetItem) -> Bool {
+        guard !Helpers.getVersionNotes(for: item, in: notes).isEmpty else {
+            return false
+        }
+
+        switch item {
+        case .currentVersion:
+            let version = Helpers.getCurrentAppVersion()
+            
+            let latestSeenVersion = UserDefaults.standard.string(
+                forKey: NOTELET_APP_STORAGE_LATEST_SEEN_APP_VERSION_KEY
+            )
+            
+            return latestSeenVersion != version
+        case .specificVersion:
+            return true
+        }
+    }
     
     func body(content: Content) -> some View {
         content
-            .sheet(item: item, onDismiss: onDismiss) { item in
+            .sheet(item: presentableItem, onDismiss: onDismiss) { item in
                 NoteletSheetContentView(
                     notes: notes,
                     sheetItem: item,
@@ -43,4 +77,3 @@ extension View {
         )
     }
 }
-
